2a93eb8f7971470835c0b17b38b079c78a4928e2
Erwin Kramer <r3verse@gmail.com>
2025-01-18 15:11:30 +0100

    format

--- Config/modules/wsl.nix
+++ Config/modules/wsl.nix
@@ -3,7 +3,7 @@
 {
   wsl.enable = true;
   wsl.defaultUser = "nixos";
-  
+
   # for vscode https://nix-community.github.io/NixOS-WSL/how-to/vscode.html
   environment.systemPackages = [
     pkgs.nixfmt-rfc-style # required for NIX IDE extension in vscode
