3342fbdd0ce35f27c2d5009fcdd538fe87f3b4f9
Erwin Kramer <r3verse@gmail.com>
2025-01-19 12:04:14 +0100

    nixpkgs pinning to 24.11 sha

--- Config/flake.nix
+++ Config/flake.nix
@@ -2,7 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/6a3ae7a5a12fb8cac2d59d7df7cbd95f9b2f0566"; # recent SHA from https://github.com/NixOS/nixpkgs/commits/nixos-24.11
+    };
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
